50ef795426451d51130245353102912ec797ce15
Skye <exp.lunate@gmail.com>
2017-03-01 13:53:24 +0000

    Create Colours

---
+++ Colour/Color3.mod.lua/_.mod.lua
@@ -0,0 +1,15 @@
+local Colours = {
+  Google = require(script.Material);
+  IBM = require(script.IBM);
+  fromHex = function(h)
+    h = h:gsub('^#','');
+    return Color3.fromRGB(
+      tonumber(h:sub(1,2), 16),
+      tonumber(h:sub(3,4), 16),
+      tonumber(h:sub(5,6), 16),
+    );
+};
+
+Colours.Material = Colours.Google;
+
+return Colours;
