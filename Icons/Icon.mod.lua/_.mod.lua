ce401271e43cf3788e77da632445fc98e8bbf40a
Skye <exp.lunate@gmail.com>
2017-03-13 14:54:42 +0000

    :tada: Icons

---
+++ Icons/Icon.mod.lua/_.mod.lua
@@ -0,0 +1,8 @@
+return {
+  -- Consider adding a lot more icons to this at some point
+  -- All return a function
+  -- Function takes an Image GuiObject, and other data
+  
+  -- (this, string SpriteSheet, string IconName)
+  Material = require(script.Material);
+}
